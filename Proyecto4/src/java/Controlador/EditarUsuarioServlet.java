a771744c87d068757837f00ff7170129bf687782
AleAle1819 <104175010+AleAle1819@users.noreply.github.com>
2023-05-10 08:46:06 -0500

    Add files via upload

---
+++ Proyecto4/src/java/Controlador/EditarUsuarioServlet.java
@@ -0,0 +1,110 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package Controlador;
+
+import Modelo.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ *
+ * @author Ale
+ */
+@WebServlet(name = "EditarUsuarioServlet", urlPatterns = {"/EditarUsuarioServlet"})
+public class EditarUsuarioServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet EditarUsuarioServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet EditarUsuarioServlet at " + request.getContextPath() + "</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+     private UsuarioD usuarioD;
+     
+    @Override
+    public void init() throws ServletException {
+        usuarioD = new UsuarioD();
+    }
+    
+        @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    if(usuarioD != null) {
+        List<Usuario> usuarios = usuarioD.seleccionarUsuarios();
+        request.setAttribute("usuarios", usuarios);        
+        request.getRequestDispatcher("editar.jsp").forward(request, response);        
+        }  
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+                int ID = Integer.getInteger(request.getParameter("ID"));//corregir
+                String nombre = request.getParameter("nombre");
+                String apellido = request.getParameter("apellido");
+                int edad = Integer.parseInt(request.getParameter("edad"));
+                String rol = request.getParameter("rol");
+                Usuario usuario = new Usuario(ID, nombre, apellido, edad, rol);
+                boolean resultado = usuarioD.editarUsuario(usuario);
+                if (resultado) {
+                    request.setAttribute("mensaje", "Usuario editado correctamente.");
+                } else {
+                    request.setAttribute("mensajeError", "Error al editar el usuario.");
+                }
+                request.getRequestDispatcher("Discord.jsp").forward(request, response);
+    }
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
